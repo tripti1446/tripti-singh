18c36eb3e11c153caf125c7dc9cb408337694311
tripti1446 <142710278+tripti1446@users.noreply.github.com>
2025-01-22 11:40:35 +0530

    Add files via upload

---
+++ whileloops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+
+    while(1==1){
+        std::cout<<"HELP! I'M STUCK IN AM INFINITE LOOP";
+    }
+
+
+    std::cout<<"hello" << name;
+
+    return 0;
+}
